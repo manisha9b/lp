b8daaa5c6ef0f81bdf2c8cb3beefe72d4bbd153b
Manisha <manisha9b@gmail.com>
2017-09-15 21:07:55 +0530

    url

--- smarantee/includes/define.php
+++ smarantee/includes/define.php
@@ -1,8 +1,5 @@
-
-application/x-httpd-php define.php ( PHP script text )
-
 <?php
 define("WEBSITE_URL","http://www.sspadvantage.com/karbonn/smarantee");
-define("SUCCESS_URL",'http://www.sspadvantage.com/karbonn/smarantee/payu/success.php');
-define("FAILURE_URL",'http://www.sspadvantage.com/karbonn/smarantee/payu/success.php');
+define("SUCCESS_URL",WEBSITE_URL.'payu/success.php');
+define("FAILURE_URL",WEBSITE_URL.'payu/success.php');
 ?>
